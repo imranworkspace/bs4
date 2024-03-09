6116aefa16086ad66a71ee8f3d8425ee9b7ee222
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-03-10 00:22:14 +0530

    update

--- flipkart_scrapping.py
+++ flipkart_scrapping.py
@@ -4,8 +4,8 @@ import requests
 from functions import bs4_functions as bs4fun
 import convertion as con
 env_vars = dotenv_values('.env')
-print('env_vars ',env_vars['amazon_path'])
-url = env_vars.get('amazon_path')
+print('env_vars ',env_vars['flipkart_path'])
+url = env_vars.get('flipkart_path')
 r = requests.get(str(url))
 soup = BeautifulSoup(r.content,'html.parser')
 titles_  = bs4fun.has_classall_para(soup,"_4rR01T")
